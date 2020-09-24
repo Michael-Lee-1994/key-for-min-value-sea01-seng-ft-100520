7ba6d0fa262a155b398904599cdf149aef7b6c66
Michael Lee <michael.l33.1994@gmail.com>
2020-09-24 22:22:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,10 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     
-      smallest_item = key if smallest_item == nil
-      smallest_value = value if smallest_value == nil
+    if smallest_item && smallest_value == nil 
+        smallest_item = key 
+        smallest_value = value 
+    end
     
     if value < smallest_value
       smallest_value = value
